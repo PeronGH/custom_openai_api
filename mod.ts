72f249656cb0b88d158b35519e927103763c2b74
peron <peron_ap@icloud.com>
2023-09-24 10:59:25 +0100

    fix: member not exported

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export { ChatCompletionSSE } from "./src/completion.ts";
+export { chatCompletionStreamResponse } from "./src/response.ts";
 export type { TiktokenModel } from "./src/tokenizer.ts";
 export { countToken } from "./src/tokenizer.ts";
 export { chatCompletion, chatCompletionChunk } from "./src/templates.ts";
